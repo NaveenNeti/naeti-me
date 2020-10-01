b8fd86bed65adb27233fb0f49199595a3b0dbbb1
Naveen Neti <naveen.neti@gmail.com>
2020-10-01 13:40:37 +1000

    Updates

---
+++ precache-manifest.77700107e1ce2b64613779bf1ebc2cde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b891f314d320fdf811924306cd097887",
+    "url": "/*https://naveenneti.github.io/naeti-me*/index.html"
+  },
+  {
+    "revision": "9d08561474ac73ef6bbd",
+    "url": "/*https://naveenneti.github.io/naeti-me*/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "10bf1bbdc9b73b25ee4e",
+    "url": "/*https://naveenneti.github.io/naeti-me*/static/js/2.558108f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/*https://naveenneti.github.io/naeti-me*/static/js/2.558108f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d08561474ac73ef6bbd",
+    "url": "/*https://naveenneti.github.io/naeti-me*/static/js/main.62fb4c90.chunk.js"
+  },
+  {
+    "revision": "560a37b857b0f77895ad",
+    "url": "/*https://naveenneti.github.io/naeti-me*/static/js/runtime-main.5f0fc32e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/*https://naveenneti.github.io/naeti-me*/static/media/logo.5d5d9eef.svg"
+  }
+]);
