a4f704a3bd19037c134c2279e897c72b55710df3
Naveen Neti <naveen.neti@gmail.com>
2020-10-01 10:23:51 +1000

    Updates

---
+++ precache-manifest.a16d557fb403885d8c8b5f0b186d03a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76c882cc706adabfd7f643a0cb2c8e87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dd1cbb4647a1644e8e9",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "10bf1bbdc9b73b25ee4e",
+    "url": "/static/js/2.558108f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.558108f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dd1cbb4647a1644e8e9",
+    "url": "/static/js/main.e29868ad.chunk.js"
+  },
+  {
+    "revision": "aff4f25750856a2f2b10",
+    "url": "/static/js/runtime-main.82204c24.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
